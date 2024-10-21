2e5f879c059415f810fd46328cd4317a1de7f761
Vincent Raoul <vincent.raoul@cyna-it.fr>
2024-10-21 15:42:39 +0200

    chore: create first name and last name validators

--- packages/common/src/validators.ts
+++ packages/common/src/validators.ts
@@ -5,3 +5,7 @@ export const urlValidator = z.string().url()
 export const emailValidator = z.string().email()
 
 export const passwordValidator = z.string().min(6)
+
+export const firstNameValidator = z.string().min(2)
+
+export const lastNameValidator = z.string().min(2)
