70e868e0e9e0774ee9bd5982d3c0f42448c32b9d
Vincent Raoul <vincent.raoul@cyna-it.fr>
2024-10-21 16:21:02 +0200

    chore: add an inferAdditionnalFields plugin in the auth client

--- packages/components/src/lib/auth.ts
+++ packages/components/src/lib/auth.ts
@@ -1,4 +1,8 @@
-import { twoFactorClient } from "better-auth/client/plugins"
+import type { auth } from "@alittlebyte/api/lib/auth"
+import {
+	inferAdditionalFields,
+	twoFactorClient,
+} from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = ({
@@ -11,6 +15,7 @@ export const authClient = ({
 	createAuthClient({
 		baseURL,
 		plugins: [
+			inferAdditionalFields<typeof auth>(),
 			twoFactorClient({
 				redirect: true,
 				twoFactorPage,
