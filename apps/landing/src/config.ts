2a2f677952a8abc7ea2ec548fca057eace3eba9c
Vincent Raoul <vincent.raoul@cyna-it.fr>
2024-10-21 16:18:42 +0200

    chore: create landing config file

---
+++ apps/landing/src/config.ts
@@ -0,0 +1,33 @@
+import { urlValidator } from "@alittlebyte/common/validators"
+import { z } from "zod"
+
+const landingConfigSchema = z.object({
+	services: z.object({
+		auth: z.object({
+			baseURL: urlValidator,
+			twoFactorPage: z.string(),
+		}),
+	}),
+})
+
+const data = {
+	services: {
+		auth: {
+			baseURL: import.meta.env.VITE_SERVICES_AUTH_BASEURL,
+			twoFactorPage: "",
+		},
+	},
+} satisfies z.input<typeof landingConfigSchema>
+
+export const landingConfig = (() => {
+	try {
+		return landingConfigSchema.parse(data)
+	} catch (error) {
+		const { errors } = error as z.ZodError
+		const formattedErrors = errors.map(
+			({ path, message }) => `\t- ${path.join(".")}: ${message}\n`,
+		)
+
+		throw new Error(`invalid config\n${formattedErrors.join("")}`)
+	}
+})()
