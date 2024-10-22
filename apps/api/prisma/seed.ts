60f009d83690094558f92e9f019023e5558d8cef
WookiesRpeople2 <keenantobiansky@gmail.com>
2024-10-22 10:20:04 +0200

    Fixed syntax and added scripts

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -1,10 +1,10 @@
-import prisma from "../../src/lib/prisma"
+import prisma from "@alittlebyte/api/lib/prisma"
 import { Prisma } from "@prisma/client"
 import { faker } from "@faker-js/faker"
 
 const MAX_ITER = 5
 
-const generateData = (): Prisma.ServiceCreateInput => ({
+const generateService = (): Prisma.ServiceCreateInput => ({
 	name: faker.commerce.productName(),
 	descriptionKey: faker.string.uuid(),
 	technicalSpecificationsKey: faker.string.uuid(),
@@ -17,9 +17,8 @@ const generateData = (): Prisma.ServiceCreateInput => ({
 ;(async () => {
 	for (let i = 0; i < MAX_ITER; i++) {
 		try {
-			const serviceData = generateData()
-			const services = await prisma.service.create({
-				data: serviceData,
+			await prisma.service.create({
+				data: generateService(),
 			})
 		} catch (error) {
 			continue // in case the name is the same
