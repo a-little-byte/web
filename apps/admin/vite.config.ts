1638f9365dee56fa09221cacd3a5dd257aebec08
Vincent Raoul <vincent.raoul@cyna-it.fr>
2024-10-24 11:39:45 +0200

    fix: admin vite config, update eslint config for the common package, and add vite.js eslint config in the package.json files list

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,9 +1,9 @@
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import eslint from "vite-plugin-eslint"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-	plugins: [eslint(), react(), tsconfigPaths(), TanStackRouterVite()],
+	// eslint-disable-next-line new-cap
+	plugins: [react(), tsconfigPaths(), TanStackRouterVite()],
 })
