85ac6afdc129d550e2ae85db72bdd1f0490b825e
Vincent Raoul <vincent.raoul@cyna-it.fr>
2024-10-24 09:43:02 +0200

    fix: lint on all vite.config.ts

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite"
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import eslint from "vite-plugin-eslint"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-	plugins: [react(), tsconfigPaths(), TanStackRouterVite()],
+	plugins: [eslint(), react(), tsconfigPaths(), TanStackRouterVite()],
 })
