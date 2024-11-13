dfda53dc9bf83cea6c0d99033d49ca9ce724bb47
Limerio <44976231+Limerio@users.noreply.github.com>
2024-11-13 15:51:20 +0100

    chore: create migrations and seed

---
+++ apps/api/src/database/seeds/1731487977273_services.ts
@@ -0,0 +1,21 @@
+import { Database, NewService } from "@alittlebyte/api/database/types"
+import { faker } from "@faker-js/faker"
+import type { Kysely } from "kysely"
+
+const MAX_ITER = 5
+const generateService = (): NewService => ({
+	name: faker.commerce.productName(),
+	descriptionKey: faker.string.uuid(),
+	technicalSpecificationsKey: faker.string.uuid(),
+	price: parseInt(faker.commerce.price(), 10),
+	perUser: faker.datatype.boolean(),
+	perDevice: faker.datatype.boolean(),
+	available: faker.datatype.boolean(),
+})
+
+export const seed = async (db: Kysely<Database>) => {
+	await db
+		.insertInto("services")
+		.values(Array(MAX_ITER).map(() => generateService()))
+		.execute()
+}
