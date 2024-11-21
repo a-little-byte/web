d44be0ea62ba9ff78f1580a24909620d7c7a1dd8
WookiesRpeople2 <keenantobiansky@gmail.com>
2024-11-21 13:48:33 +0100

    chore: deleted div

--- packages/components/src/ui/hero.tsx
+++ packages/components/src/ui/hero.tsx
@@ -14,16 +14,14 @@ export const Hero: FC<PropsWithChildren<HeroProps>> = ({
 }) => (
 	<div className="relative h-96">
 		<img src={imageUrl} alt="Hero" className="h-full w-full object-cover" />
-		<div>
-			<div className="absolute inset-0 flex items-center bg-black bg-opacity-50">
-				<div className="container mx-auto px-6">
-					<h1 className="mb-4 text-4xl font-bold text-white md:text-6xl">
-						{title}
-					</h1>
-					<p className="font-bold text-white">{description}</p>
-				</div>
-				{children}
+		<div className="absolute inset-0 flex items-center bg-black bg-opacity-50">
+			<div className="container mx-auto px-6">
+				<h1 className="mb-4 text-4xl font-bold text-white md:text-6xl">
+					{title}
+				</h1>
+				<p className="font-bold text-white">{description}</p>
 			</div>
+			{children}
 		</div>
 	</div>
 )
