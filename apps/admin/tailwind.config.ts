ea11d3c36a37d4390a16fcf6ef62f3e904b94bb8
Limerio <44976231+Limerio@users.noreply.github.com>
2025-01-07 14:58:54 +0100

    chore: remove the admin app

--- apps/admin/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import tailwindPreset from "@alittlebyte/components/tailwind.config"
-import type { Config } from "tailwindcss"
-
-const config: Config = {
-	presets: [tailwindPreset],
-	content: ["src/**/*.{ts,tsx}", "../../packages/components/src/**/*.{ts,tsx}"],
-}
-
-export default config
