449d3a5753e48de7481b6760e097376ac9e544fb
Limerio <44976231+Limerio@users.noreply.github.com>
2025-01-08 09:19:41 +0100

    fix: issues

--- apps/landing/src/routes/forgot-password.lazy.tsx
+++ apps/landing/src/routes/forgot-password.lazy.tsx
@@ -2,50 +2,51 @@ import {
 	ForgotPasswordForm,
 	ForgotPasswordValidatorOutput,
 } from "@alittlebyte/components/forms/ForgotPasswordForm"
+import { useToast } from "@alittlebyte/components/hooks/use-toast"
 import { useAuthClient } from "@alittlebyte/landing/hooks/useAuthClient"
 import { useMutation } from "@tanstack/react-query"
 import { createLazyFileRoute, useNavigate } from "@tanstack/react-router"
-import { useCallback } from "react"
 import { SubmitHandler } from "react-hook-form"
-import { useToast } from "@alittlebyte/components/hooks/use-toast"
 
 const ForgotPassword = () => {
 	const { forgetPassword } = useAuthClient()
 	const { toast } = useToast()
 	const navigate = useNavigate()
-	const { mutate } = useMutation<unknown, Error, ForgotPasswordValidatorOutput>(
-		{
-			mutationFn: async ({ email }) => {
-				const res = await forgetPassword({
-					email,
-					redirectTo: `${window.location.origin}/reset-password`,
-				})
+	const { mutateAsync } = useMutation<
+		unknown,
+		Error,
+		ForgotPasswordValidatorOutput
+	>({
+		mutationFn: async ({ email }) => {
+			const res = await forgetPassword({
+				email,
+				redirectTo: `${window.location.origin}/reset-password`,
+			})
 
-				if (res.error) {
-					throw new Error(res.error.message)
-				}
-			},
-			onSuccess: async () => {
-				toast({
-					title: "Please check your email",
-					description: "An email has been sent in order to reset your password",
-				})
-				await navigate({ to: "/" })
-			},
-			onError: (error) => {
-				toast({
-					title: "An error occured",
-					description: error.message,
-				})
-			},
+			if (res.error) {
+				throw new Error(res.error.message)
+			}
 		},
-	)
-	const onSubmit = useCallback<SubmitHandler<ForgotPasswordValidatorOutput>>(
-		(values) => {
-			mutate(values)
+		onSuccess: async () => {
+			toast({
+				title: "Please check your email",
+				description: "An email has been sent in order to reset your password",
+			})
+			await navigate({ to: "/" })
 		},
-		[mutate],
-	)
+		onError: (error) => {
+			toast({
+				variant: "destructive",
+				title: "An error occurred",
+				description: error.message,
+			})
+		},
+	})
+	const onSubmit: SubmitHandler<ForgotPasswordValidatorOutput> = async (
+		values,
+	) => {
+		await mutateAsync(values)
+	}
 
 	return (
 		<div className="p-2">
--- packages/components/src/ui/AvailableBadge.tsx
+++ packages/components/src/ui/AvailableBadge.tsx
@@ -1,6 +1,6 @@
 import { Badge } from "@alittlebyte/components/ui/badge"
 
-export const AvalibleBadge = ({ available }: { available: boolean }) => (
+export const AvailableBadge = ({ available }: { available: boolean }) => (
 	<Badge variant={available ? "default" : "destructive"} className="text-sm">
 		{available ? "Available" : "Unavailable"}
 	</Badge>
