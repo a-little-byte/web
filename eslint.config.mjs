02af0bc2eba00e05cdcd08020e24a962c70ae2de
Limerio <44976231+Limerio@users.noreply.github.com>
2025-03-23 16:15:24 +0100

    chore(eslint): migrate ESLint configuration to new format and update lint scripts

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import { FlatCompat } from "@eslint/eslintrc";
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+const eslintConfig = [
+  ...pluginQuery.configs["flat/recommended"],
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-empty-interface": "off",
+    },
+  }),
+];
+export default eslintConfig;
