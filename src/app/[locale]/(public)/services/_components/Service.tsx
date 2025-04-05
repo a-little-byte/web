af54a366fe3cb3e9336ed029c84a017a5af0842d
Limerio <44976231+Limerio@users.noreply.github.com>
2025-04-05 14:52:41 +0200

    feat(services): replace Services component with Service component for individual service rendering

--- src/app/[locale]/(public)/services/_components/Service.tsx
+++ src/app/[locale]/(public)/services/_components/Service.tsx
@@ -1,23 +1,9 @@
 "use client";
 
-import { AddToCartForm } from "@/app/[locale]/(public)/services/_components/AddToCartForm";
 import type { ServiceSelect } from "@/db/models/Service";
-import { useForm } from "@/hooks/useForm";
-import { UseFormReturn } from "react-hook-form";
-import { z } from "zod";
+import { Check, Lock, Shield, Zap } from "lucide-react";
 
-export const servicesFormSchema = z.object({
-  durations: z.record(z.string(), z.string()),
-});
-
-export const Services = ({ services }: { services: ServiceSelect[] }) => {
-  const form = useForm(servicesFormSchema);
-
-  return services.map((service) => (
-    <Service key={service.id} service={service} form={form} />
-  ));
-};
-
+import { useTranslations } from "next-intl";
 export const durationOptions = [
   { value: "1", multiplier: 1, discount: 0 },
   { value: "3", multiplier: 3, discount: 0.1 },
@@ -33,19 +19,14 @@ const calculatePrice = (basePrice: number, duration: string) => {
   return totalBeforeDiscount - discount;
 };
 
-export const Service = ({
-  service,
-  form,
-}: {
-  service: ServiceSelect;
-  form: UseFormReturn<
-    z.input<typeof servicesFormSchema>,
-    unknown,
-    z.output<typeof servicesFormSchema>
-  >;
-}) => {
-  const duration = form.watch("durations")[service.id];
-  const price = calculatePrice(service.price, duration);
+const icons = {
+  MDR: <Shield className="h-8 w-8 text-primary" />,
+  EDR: <Zap className="h-8 w-8 text-primary" />,
+  XDR: <Lock className="h-8 w-8 text-primary" />,
+} as const;
+
+export const Service = ({ service }: { service: ServiceSelect }) => {
+  const t = useTranslations("services");
 
   return (
     <div
@@ -54,31 +35,29 @@ export const Service = ({
     >
       <div>
         <div className="flex items-center gap-x-4">
-          {/* <service.icon className="h-8 w-8 text-primary" /> */}
+          {icons[service.name as keyof typeof icons]}
           <h3 className="text-lg font-semibold leading-8">{service.name}</h3>
         </div>
         <p className="mt-4 text-sm leading-6 text-muted-foreground">
           {service.description}
         </p>
         <ul role="list" className="mt-8 space-y-3 text-sm leading-6">
-          {/* {service.features.map((feature: string) => (
+          {service.features.map((feature: string) => (
             <li key={feature} className="flex gap-x-3">
               <Check className="h-5 w-5 text-primary" />
-              {feature}
+              {t(`products.${service.name}.features.${feature}`)}
             </li>
-          ))} */}
+          ))}
         </ul>
       </div>
 
       <div className="mt-4">
         <div className="text-2xl font-bold mb-3">
-          ${price.toFixed(2)}{" "}
+          ${service.price}{" "}
           <span className="text-sm text-muted-foreground/90">
             / {service.period}
           </span>
         </div>
-
-        <AddToCartForm service={service} form={form} />
       </div>
     </div>
   );
