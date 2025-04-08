56a1e4555be08c57802f7d861f46b681e559b710
Limerio <44976231+Limerio@users.noreply.github.com>
2025-04-08 14:32:08 +0200

    feat: add password_salt column to users table and implement down migration

--- migrations/1743513255090_add_password_salt.ts
+++ migrations/1743513255090_add_password_salt.ts
@@ -1,10 +1,12 @@
-import type { Kysely } from 'kysely'
+import type { Kysely } from "kysely";
 
 export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
-     .alterTable("users")
-     .addColumn("password_salt", "text", (col) => col.notNull())
-     .execute()
+    .alterTable("users")
+    .addColumn("password_salt", "text", (col) => col.notNull())
+    .execute();
 }
 
-
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.alterTable("users").dropColumn("password_salt").execute();
+}
