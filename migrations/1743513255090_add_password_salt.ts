c4f0abdd4c85ea811d924d21cf0b6afd875857bf
WookiesRpeople2 <keentobiansky@gmail.com>
2025-04-08 14:42:38 +0200

    chore: add types for add password salt migration

--- migrations/1743513255090_add_password_salt.ts
+++ migrations/1743513255090_add_password_salt.ts
@@ -1,12 +1,13 @@
+import { Database } from "@/db";
 import type { Kysely } from "kysely";
 
-export async function up(db: Kysely<any>): Promise<void> {
+export async function up(db: Kysely<Database>): Promise<void> {
   await db.schema
     .alterTable("users")
     .addColumn("password_salt", "text", (col) => col.notNull())
     .execute();
 }
 
-export async function down(db: Kysely<any>): Promise<void> {
+export async function down(db: Kysely<Database>): Promise<void> {
   await db.schema.alterTable("users").dropColumn("password_salt").execute();
 }
