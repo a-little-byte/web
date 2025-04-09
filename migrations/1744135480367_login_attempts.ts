bb37b1512d23e7aa76a5ee995fea433ac6e53830
Limerio <44976231+Limerio@users.noreply.github.com>
2025-04-09 12:30:00 +0200

    refactor: simplify migration function syntax and improve code formatting for login_attempts table

--- migrations/1744135480367_login_attempts.ts
+++ migrations/1744135480367_login_attempts.ts
@@ -1,21 +1,22 @@
-import { Database } from '@/db';
-import { sql, type Kysely } from 'kysely'
+import { Database } from "@/db";
+import { sql, type Kysely } from "kysely";
 
-export async function up(db: Kysely<Database>): Promise<void> {
+export const up = async (db: Kysely<Database>): Promise<void> => {
   await db.schema
-  .createTable("login_attempts")
-  .addColumn("id", "uuid", (col) =>
+    .createTable("login_attempts")
+    .addColumn("id", "uuid", (col) =>
       col.primaryKey().defaultTo(sql`gen_random_uuid()`)
-   )
-  .addColumn("email", "text", (col) => col.notNull())
-  .addColumn("ip_address", "text", (col) => col.notNull())
-  .addColumn("attempted_at", "timestamptz", (col) => col.defaultTo(sql`now()`))
-  .addColumn("is_locked", "boolean", (col) => col.defaultTo(false))
-  .addColumn("lock_expires_at", "timestamp")
-  .execute();
+    )
+    .addColumn("email", "text", (col) => col.notNull())
+    .addColumn("ip_address", "text", (col) => col.notNull())
+    .addColumn("attempted_at", "timestamptz", (col) =>
+      col.defaultTo(sql`now()`)
+    )
+    .addColumn("is_locked", "boolean", (col) => col.defaultTo(false))
+    .addColumn("lock_expires_at", "timestamp")
+    .execute();
+};
 
-}
-
-export async function down(db: Kysely<Database>): Promise<void> {
-  await db.schema.dropTable("login_attempts").execute()
-}
+export const down = async (db: Kysely<Database>): Promise<void> => {
+  await db.schema.dropTable("login_attempts").execute();
+};
