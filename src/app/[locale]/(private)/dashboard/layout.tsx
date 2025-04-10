f136cacd2737b37955e75d6a39dd7eb45bbb0c07
Limerio <44976231+Limerio@users.noreply.github.com>
2025-04-10 16:40:10 +0200

    feat: refactor dashboard layout to integrate new AppSidebar component and streamline sidebar functionality

--- src/app/[locale]/(private)/dashboard/layout.tsx
+++ src/app/[locale]/(private)/dashboard/layout.tsx
@@ -1,50 +1,12 @@
 "use client";
 
-import ScrambleHover from "@/components/ui/scramble";
-import { Link, usePathname, useRouter } from "@/lib/i18n/routing";
-import { cn } from "@/lib/style";
-import { CreditCard, Home, Settings } from "lucide-react";
-import { useTranslations } from "next-intl";
-import { ReactNode, useEffect, useMemo } from "react";
+import { AppSidebar } from "@/components/layout/Sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { useRouter } from "@/lib/i18n/routing";
+import { useEffect } from "react";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
-  const t = useTranslations("navigation");
-  const pathname = usePathname();
-
-  const SIDEBAR: Array<{ href: string; children: ReactNode }> = useMemo(
-    () => [
-      {
-        href: "/dashboard",
-        children: (
-          <>
-            <Home className="h-4 w-4" />
-            {t("overview")}
-          </>
-        ),
-      },
-      {
-        href: "/dashboard/order-history",
-        children: (
-          <>
-            <CreditCard className="h-4 w-4" />
-            {t("orderHistory")}
-          </>
-        ),
-      },
-      {
-        href: "/dashboard/settings",
-        children: (
-          <>
-            <Settings className="h-4 w-4" />
-            {t("settings")}
-          </>
-        ),
-      },
-    ],
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    []
-  );
 
   useEffect(() => {
     const token = document.cookie.replace(
@@ -61,32 +23,13 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <div className="flex min-h-screen">
-      <aside className="w-64 bg-card border-r">
-        <nav className="p-4 space-y-2">
-          <h2 className="text-lg font-semibold mb-4">
-            <ScrambleHover
-              text={t("dashboard")}
-              sequential={true}
-              revealDirection="start"
-              useOriginalCharsOnly={false}
-              className="font-azeretMono"
-            />
-          </h2>
-          {SIDEBAR.map(({ href, children: childrenLink }) => (
-            <Link
-              key={href}
-              href={href}
-              className={cn(
-                "flex items-center gap-2 p-2 hover:bg-muted rounded-md",
-                pathname === href && "bg-muted"
-              )}
-            >
-              {childrenLink}
-            </Link>
-          ))}
-        </nav>
-      </aside>
-      <main className="flex-1 p-8">{children}</main>
+      <SidebarProvider>
+        <AppSidebar />
+        <main className="flex-1 px-8 py-4">
+          <SidebarTrigger className="mb-4" />
+          {children}
+        </main>
+      </SidebarProvider>
     </div>
   );
 };
---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,113 @@
+import { CynaIcon } from "@/components/icons/Cyna";
+import ScrambleHover from "@/components/ui/scramble";
+import {
+  Sidebar as BaseSidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { Link, usePathname, useRouter } from "@/lib/i18n/routing";
+import { CreditCard, Home, Search, Settings } from "lucide-react";
+import { useTranslations } from "next-intl";
+import { ReactNode, useCallback, useMemo } from "react";
+
+export const AppSidebar = () => {
+  const t = useTranslations("navigation");
+  const router = useRouter();
+  const pathname = usePathname();
+  const SIDEBAR: Array<{ href: string; children: ReactNode }> = useMemo(
+    () => [
+      {
+        href: "/dashboard",
+        children: (
+          <>
+            <Home className="h-4 w-4" />
+            <span>{t("overview")}</span>
+          </>
+        ),
+      },
+      {
+        href: "/dashboard/order-history",
+        children: (
+          <>
+            <CreditCard className="h-4 w-4" />
+            <span>{t("orderHistory")}</span>
+          </>
+        ),
+      },
+      {
+        href: "/dashboard/settings",
+        children: (
+          <>
+            <Settings className="h-4 w-4" />
+            <span>{t("settings")}</span>
+          </>
+        ),
+      },
+    ],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+  const onClick = useCallback(
+    (href: string) => () => {
+      router.push(href);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
+  return (
+    <BaseSidebar>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+              asChild
+            >
+              <Link href="/dashboard">
+                <CynaIcon className="h-11 w-11" />
+                <span className="text-lg font-semibold">
+                  <ScrambleHover
+                    text="Cyna"
+                    sequential={true}
+                    revealDirection="start"
+                    useOriginalCharsOnly={false}
+                  />
+                </span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent className="flex flex-col gap-2">
+            <SidebarMenu>
+              <SidebarMenuItem className="flex items-center gap-2">
+                <SidebarMenuButton className="min-w-8 py-4 mb-3 bg-[#1c1cae] text-primary-foreground duration-200 ease-linear hover:bg-primary hover:cursor-pointer">
+                  <Search className="h-4 w-4" />
+                  <span>Search</span>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              {SIDEBAR.map((item) => (
+                <SidebarMenuItem key={item.href}>
+                  <SidebarMenuButton
+                    isActive={pathname === item.href}
+                    onClick={onClick(item.href)}
+                  >
+                    {item.children}
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </BaseSidebar>
+  );
+};
