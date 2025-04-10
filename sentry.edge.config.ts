cd9b326864ebf13e50fe7bb1c9e6b59092f75d2a
Limerio <44976231+Limerio@users.noreply.github.com>
2025-04-10 10:04:46 +0200

    chore: update Sentry DSN environment variable in configuration files and add client-side instrumentation for Sentry

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -1,8 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  dsn: process.env.SENTRY_DSN,
   tracesSampleRate: 1,
   debug: false,
-  enabled: process.env.NEXT_PUBLIC_NODE_ENV === "production",
 });
