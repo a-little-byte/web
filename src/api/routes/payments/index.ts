37046fb04cd617f79b708f9445bc53cb3a535d98
Limerio <44976231+Limerio@users.noreply.github.com>
2025-04-29 11:40:27 +0200

    refactor: consolidate payment methods API routes into a single endpoint, enhance validation, and improve response structure

--- src/api/routes/payments/index.ts
+++ src/api/routes/payments/index.ts
@@ -4,47 +4,44 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { jsonObjectFrom } from "kysely/helpers/postgres";
 import { z } from "zod";
-import { paymentMethodsRouter } from "./payment_methods";
 
 export const paymentsRouter = new Hono<{
   Variables: PrivateContextVariables;
-}>()
-  .get(
-    "/",
-    checkPermissions("payments.read"),
-    zValidator(
-      "query",
-      z.object({
-        startDate: z.string().datetime(),
-        endDate: z.string().datetime(),
-      }),
-    ),
-    async ({ var: { db }, json, req }) => {
-      const { startDate, endDate } = req.valid("query");
-      const payments = await db
-        .selectFrom("payments")
-        .selectAll()
-        .where("createdAt", ">=", new Date(startDate))
-        .where("createdAt", "<=", new Date(endDate))
-        .select((eb) =>
-          jsonObjectFrom(
-            eb
-              .selectFrom("subscriptions")
-              .selectAll(["subscriptions"])
-              .select((eb) =>
-                jsonObjectFrom(
-                  eb
-                    .selectFrom("services")
-                    .selectAll(["services"])
-                    .whereRef("services.id", "=", "subscriptions.service_id"),
-                ).as("service"),
-              )
-              .whereRef("subscriptions.id", "=", "payments.subscription_id"),
-          ).as("subscription"),
-        )
-        .execute();
+}>().get(
+  "/",
+  checkPermissions("payments.read"),
+  zValidator(
+    "query",
+    z.object({
+      startDate: z.string().datetime(),
+      endDate: z.string().datetime(),
+    })
+  ),
+  async ({ var: { db }, json, req }) => {
+    const { startDate, endDate } = req.valid("query");
+    const payments = await db
+      .selectFrom("payments")
+      .selectAll()
+      .where("createdAt", ">=", new Date(startDate))
+      .where("createdAt", "<=", new Date(endDate))
+      .select((eb) =>
+        jsonObjectFrom(
+          eb
+            .selectFrom("subscriptions")
+            .selectAll(["subscriptions"])
+            .select((eb) =>
+              jsonObjectFrom(
+                eb
+                  .selectFrom("services")
+                  .selectAll(["services"])
+                  .whereRef("services.id", "=", "subscriptions.service_id")
+              ).as("service")
+            )
+            .whereRef("subscriptions.id", "=", "payments.subscription_id")
+        ).as("subscription")
+      )
+      .execute();
 
-      return json(payments);
-    },
-  )
-  .route("/payment-methodes", paymentMethodsRouter);
+    return json(payments);
+  }
+);
